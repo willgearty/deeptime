89b28316f360e40fa3af75236c18c040c50b3616
willgearty@gmail.com <willgearty@gmail.com>
2018-10-12 16:52:47 -0700

    Added example. Rearranged arguments. Added rotation argument.

--- R/gggeo_scale.R
+++ R/gggeo_scale.R
@@ -9,6 +9,8 @@
 #'   The \code{abbr} column is optonal and lists abbreviations that may be used as labels.
 #'   The \code{color} column is also optional and lists a hex color code (which can be obtained with \code{rgb()}) for each time interval.
 #' @param gg The ggplot object.
+#' @param dat Either A) a string indicating a built-in dataframe with interval data (periods, epochs, or stages)
+#'   or B) a custom dataframe of time interval boundaries (see Details).
 #' @param fill The fill color of the boxes. The default is to use the colors included in \code{dat}.
 #'   If a custom dataset is provided with dat without color and without fill, a greyscale will be used
 #'   Custom fill colors can be provided with this option and will be recycled if/as necessary.
@@ -16,13 +18,12 @@
 #' @param alpha The transparency of the fill colors.
 #' @param height The proportional height (or width if \code{pos} is \code{left} or \code{right}) of the entire plot to use for the scale.
 #' @param gap The proportional height (or width) of the entire plot to use as a gap between the axis and the scale.
-#' @param size Label size.
-#' @param quat Specifies whether the Quaternary should be labelled (if using the default data).
 #' @param pos Which side to add the scale to (left, right, top, or bottom). First letter may also be used.
 #' @param lab Whether to include labels.
+#' @param rot The amount of counter-clockwise rotation to add to the labels (in degrees).
 #' @param abbrv If including labels, whether to use abbreviations instead of full interval names.
-#' @param dat Either A) a string indicating a built-in dataframe with interval data (periods, epochs, or stages)
-#'   or B) a custom dataframe of time interval boundaries (see Details).
+#' @param skip A vector of interval names indicating which intervals should not be labelled.
+#' @param size Label size.
 #' @param neg Set this to true if your x-axis is using negative values.
 #' @return A ggplot object.
 #' @export
@@ -44,6 +45,16 @@
 #'  theme_classic()
 #' gggeo_scale(p, pos = "left")
 #'
+#' # can add multiple scales
+#' p <- ggplot() +
+#'   geom_point(aes(y = runif(1000, 1, 8), x = runif(1000, 0, 1000))) +
+#'   scale_x_reverse() +
+#'   coord_cartesian(xlim = c(0, 1000), ylim = c(0,8), expand = FALSE) +
+#'   theme_classic()
+#' p <- gggeo_scale(p, height = .03)
+#' p <- gggeo_scale(p, gap = .03, height = .03, dat = "epochs")
+#' gggeo_scale(p, gap = .06, height = .03, dat = "stages")
+#'
 #' #can even add a scale to a phylogeny (using ggtree)
 #' library(phytools)
 #' library(ggtree)
@@ -54,7 +65,7 @@
 #'  theme_tree2()
 #' p <- revts(p)
 #' gggeo_scale(p, neg = TRUE)
-gggeo_scale <- function(gg, fill = NULL, color = "black", alpha = 1, height = .05, gap = 0, size = 5, quat = FALSE, pos = "bottom", lab = TRUE, abbrv = TRUE, dat = "periods", neg = FALSE) {
+gggeo_scale <- function(gg, dat = "periods", fill = NULL, color = "black", alpha = 1, height = .05, gap = 0, pos = "bottom", lab = TRUE, rot = 0, abbrv = TRUE, skip = c("Quaternary", "Holocene"), size = 5, neg = FALSE) {
   if(dat == "periods"){
     dat <- periods
   }else if(dat == "epochs"){
@@ -78,9 +89,7 @@ gggeo_scale <- function(gg, fill = NULL, color = "black", alpha = 1, height = .0
   }else{
     dat$label <- dat$name
   }
-  if(!quat){
-    dat$label[dat$abbr=="Q"] <- ""
-  }
+  dat$label[dat$name %in% skip] <- ""
   if(pos %in% c("bottom", "top", "b", "t")){
     y.range <- max(lims$y.range) - min(lims$y.range)
     if(pos %in% c("top","t")){
@@ -95,7 +104,7 @@ gggeo_scale <- function(gg, fill = NULL, color = "black", alpha = 1, height = .0
                         fill = dat$color, color = color, alpha = alpha)
     if(lab){
       gg <- gg + ggplot2::annotate("text", x = dat$mid_age, label = dat$label, y = (ymin+ymax)/2,
-                                   vjust = "middle", hjust = "middle", size = size)
+                                   vjust = "middle", hjust = "middle", size = size, angle = rot)
     }
   }else if(pos %in% c("left", "right","l","r")){
     x.range <- max(lims$x.range) - min(lims$x.range)
@@ -111,7 +120,7 @@ gggeo_scale <- function(gg, fill = NULL, color = "black", alpha = 1, height = .0
                         fill = dat$color, color = color, alpha = alpha)
     if(lab){
       gg <- gg + ggplot2::annotate("text", y = dat$mid_age, label = dat$label, x = (xmin+xmax)/2,
-                                   vjust = "middle", hjust = "middle", size = size, angle = 90)
+                                   vjust = "middle", hjust = "middle", size = size, angle = 90 + rot)
     }
   }
   gg
