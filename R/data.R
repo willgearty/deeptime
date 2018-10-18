7f87cd7a41a8f60cd3d9bbdab87044f6200ee4fc
willgearty@gmail.com <willgearty@gmail.com>
2018-10-18 14:29:49 -0700

    Added eon and era data

--- R/data.R
+++ R/data.R
@@ -19,7 +19,7 @@
 #' A dataset containing the boundary ages, abbrevations, and colors for the stages of the Geologic Time Scale.
 #' Based on The Geologic Time Scale 2012, by Gradstein and Ogg.
 #'
-#' @format A data frame with 15 rows and 5 variables:
+#' @format A data frame with 97 rows and 4 variables:
 #' \describe{
 #'   \item{name}{stage name}
 #'   \item{max_age}{maximum age, in millions of years}
@@ -34,7 +34,7 @@
 #' A dataset containing the boundary ages, abbrevations, and colors for the epochs of the Geologic Time Scale.
 #' Based on The Geologic Time Scale 2012, by Gradstein and Ogg.
 #'
-#' @format A data frame with 15 rows and 5 variables:
+#' @format A data frame with 34 rows and 4 variables:
 #' \describe{
 #'   \item{name}{epoch name}
 #'   \item{max_age}{maximum age, in millions of years}
@@ -43,3 +43,33 @@
 #' }
 #' @source \url{https://doi.org/10.1016/C2011-1-08249-8}
 "epochs"
+
+#' Era data based on The Geologic Time Scale 2012
+#'
+#' A dataset containing the boundary ages, abbrevations, and colors for the eras of the Geologic Time Scale.
+#' Based on The Geologic Time Scale 2012, by Gradstein and Ogg.
+#'
+#' @format A data frame with 11 rows and 4 variables:
+#' \describe{
+#'   \item{name}{era name}
+#'   \item{max_age}{maximum age, in millions of years}
+#'   \item{min_age}{minimum age, in millions of years}
+#'   \item{color}{the colors for each era, according to the Comission for the Geological Map of the World}
+#' }
+#' @source \url{https://doi.org/10.1016/C2011-1-08249-8}
+"eras"
+
+#' Eon data based on The Geologic Time Scale 2012
+#'
+#' A dataset containing the boundary ages, abbrevations, and colors for the eons of the Geologic Time Scale.
+#' Based on The Geologic Time Scale 2012, by Gradstein and Ogg.
+#'
+#' @format A data frame with 4 rows and 4 variables:
+#' \describe{
+#'   \item{name}{eon name}
+#'   \item{max_age}{maximum age, in millions of years}
+#'   \item{min_age}{minimum age, in millions of years}
+#'   \item{color}{the colors for each eon, according to the Comission for the Geological Map of the World}
+#' }
+#' @source \url{https://doi.org/10.1016/C2011-1-08249-8}
+"eons"
