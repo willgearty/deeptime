a02060016395381d8fd445294628575bd8d594b3
Will Gearty <willgearty@gmail.com>
2019-07-19 11:29:48 -0700

    Fix treatment of generically named stages. Update use_data() source.

--- data-raw/ICS_2013.R
+++ data-raw/ICS_2013.R
@@ -6,29 +6,31 @@ colnames(clean_dat) <- c("name", "max_age", "min_age", "abbr", "color", "scale_l
 #Eon data
 eons <- within(subset(clean_dat, scale_level == 1), rm(scale_level))
 no_abbr <- (is.na(eons$abbr) | eons$abbr == "")
-eons$abbr[no_abbr] <- abbreviate(eons$name, minlength = 1, use.classes = FALSE, named = FALSE)[no_abbr]
-devtools::use_data(eons, overwrite = TRUE)
+eons$abbr[no_abbr] <- abbreviate(eons$name, minlength = 1, use.classes = TRUE, named = FALSE)[no_abbr]
+usethis::use_data(eons, overwrite = TRUE)
 
 #Era data
 eras <- within(subset(clean_dat, scale_level == 2), rm(scale_level))
 no_abbr <- (is.na(eras$abbr) | eras$abbr == "")
-eras$abbr[no_abbr] <- abbreviate(eras$name, minlength = 1, use.classes = FALSE, named = FALSE)[no_abbr]
-devtools::use_data(eras, overwrite = TRUE)
+eras$abbr[no_abbr] <- abbreviate(eras$name, minlength = 1, use.classes = TRUE, named = FALSE)[no_abbr]
+usethis::use_data(eras, overwrite = TRUE)
 
 #Period data
 periods <- within(subset(clean_dat, scale_level == 3), rm(scale_level))
 no_abbr <- (is.na(periods$abbr) | periods$abbr == "")
-periods$abbr[no_abbr] <- abbreviate(periods$name, minlength = 1, use.classes = FALSE, named = FALSE)[no_abbr]
-devtools::use_data(periods, overwrite = TRUE)
+periods$abbr[no_abbr] <- abbreviate(periods$name, minlength = 1, use.classes = TRUE, named = FALSE)[no_abbr]
+usethis::use_data(periods, overwrite = TRUE)
 
 #Epoch data
 epochs <- within(subset(clean_dat, scale_level == 4), rm(scale_level))
 no_abbr <- (is.na(epochs$abbr) | epochs$abbr == "")
-epochs$abbr[no_abbr] <- abbreviate(epochs$name, minlength = 1, use.classes = FALSE, named = FALSE)[no_abbr]
-devtools::use_data(epochs, overwrite = TRUE)
+epochs$abbr[no_abbr] <- abbreviate(epochs$name, minlength = 1, use.classes = TRUE, named = FALSE)[no_abbr]
+usethis::use_data(epochs, overwrite = TRUE)
 
 #Stage data
 stages <- within(subset(clean_dat, scale_level == 5), rm(scale_level))
 no_abbr <- (is.na(stages$abbr) | stages$abbr == "")
-stages$abbr[no_abbr] <- abbreviate(stages$name, minlength = 1, use.classes = FALSE, named = FALSE)[no_abbr]
-devtools::use_data(stages, overwrite = TRUE)
+extra_stages <- paste("Stage",1:10)
+extra_stages <- extra_stages[!extra_stages %in% stages$name]
+stages$abbr[no_abbr] <- abbreviate(c(stages$name, extra_stages), minlength = 1, use.classes = TRUE, named = FALSE)[which(no_abbr)]
+usethis::use_data(stages, overwrite = TRUE)
