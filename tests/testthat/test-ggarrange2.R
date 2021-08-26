74b2082421111ebbe460d27a75c3b4fd6647842d
Will Gearty <willgearty@gmail.com>
2021-08-26 11:53:21 -0500

    Expand ggarrange2() tests

--- tests/testthat/test-ggarrange2.R
+++ tests/testthat/test-ggarrange2.R
@@ -7,7 +7,7 @@ test_that("ggarrange2() works", {
     geom_point(aes(x = Log_D, y = log_dur)) +
     labs(x = "Body size", y = "Stratigraphic duration (myr)") +
     theme_classic()
-  gg1 <- ggarrange2(p1, p2, widths = c(2,1), draw = FALSE)
+  gg1 <- ggarrange2(p1, p2, widths = c(2,1), draw = FALSE, labels = c("A", "B"))
   expect_true(gtable::is.gtable(gg1))
   expect_doppelganger("ggarrange2()", gg1)
   expect_doppelganger("ggarrange2() layout", gtable::gtable_show_layout(gg1))
@@ -20,7 +20,8 @@ test_that("ggarrange2() works", {
     theme_tree2() +
     theme(plot.margin = margin(7,11,7,11))
 
-  gg2 <- ggarrange2(gg1, p3, nrow = 2, heights = c(1,2), draw = FALSE)
+  gg2 <- ggarrange2(gg1, p3, nrow = 2, heights = c(1,2), draw = FALSE,
+                    top = "Test1", bottom = "Test2", left = "Test3", right = "Test4")
   expect_true(gtable::is.gtable(gg2))
   expect_doppelganger("double ggarrange2()", gg2)
   expect_doppelganger("double ggarrange2() layout", gtable::gtable_show_layout(gg2))
