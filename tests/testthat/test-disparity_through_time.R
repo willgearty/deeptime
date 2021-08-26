3f55c4a5796ac18b8f1ec0a5bce587f5a292c262
Will Gearty <willgearty@gmail.com>
2021-08-26 12:22:07 -0500

    Add tests for disparity_through_time()

---
+++ tests/testthat/test-disparity_through_time.R
@@ -0,0 +1,13 @@
+test_that("disparity_through_time() works", {
+  suppressPackageStartupMessages(library(dispRity, quietly = TRUE))
+  data(demo_data)
+  crinoids <- as.data.frame(demo_data$wright$matrix[[1]][, 1:2])
+  crinoids$time <- "before extinction"
+  crinoids$time[demo_data$wright$subsets$after$elements] <- "after extinction"
+  crinoids$time <- factor(crinoids$time)
+  attach(crinoids)
+  gg <- disparity_through_time(time~V1*V2, groups = time, aspect = c(2,1), xlim = c(-.6,.6), ylim = c(-.5,.5),
+                         col.regions = "lightgreen", col.point = c("red","blue"))
+  expect_true(is(gg, "trellis"))
+  expect_doppelganger("disparity_through_time()", gg)
+})
