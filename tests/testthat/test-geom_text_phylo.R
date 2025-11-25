fa0cf026c2bf734e39c6fa2c2e503663d2890953
William Gearty <willgearty@gmail.com>
2025-11-25 11:21:14 -0500

    Add tests

--- tests/testthat/test-geom_text_phylo.R
+++ tests/testthat/test-geom_text_phylo.R
@@ -24,3 +24,42 @@ test_that("geom_text_phylo works", {
       coord_geo_radial("epochs")
   })
 })
+
+test_that("geom_text_clade works", {
+  skip_if_not_installed("ape")
+  skip_if_not_installed("ggtree")
+  skip_if_not_installed("phytools")
+  library(ape)
+  library(ggtree)
+  library(phytools)
+  data(primate.tree)
+  clades.df <- data.frame(
+    clade = c("Lorisoidea", "Lemuroidea", "Tarsioidea", "Ceboidea",
+              "Cercopithecoidea", "Hominoidea"),
+    node = c(166, 146, 144, 120, 95, 114)
+  )
+  gg <- revts(ggtree(primate.tree)) %<+% clades.df +
+    geom_text_clade(aes(label = clade), extend = c(0.1, 0.1)) +
+    coord_geo_radial()
+  expect_true(is_ggplot(gg))
+  expect_true(is(gg$layers[[3]]$geom, "GeomTextClade"))
+  expect_true(is(gg$layers[[3]]$stat, "StatIdentity"))
+  expect_doppelganger_deeptime("geom_text_clade", gg)
+
+  gg <- revts(ggtree(primate.tree)) %<+% clades.df +
+    geom_text_clade(aes(label = clade), extend = c(0.1, 0.1),
+                    text_geom = "label") +
+    coord_geo_radial()
+  expect_doppelganger_deeptime("geom_text_clade-labels", gg)
+
+  expect_error({
+    revts(ggtree(primate.tree)) %<+% clades.df +
+      geom_text_clade(aes(label = clade), text_geom = "rect") +
+      coord_geo_radial()
+  })
+  expect_error({
+    revts(ggtree(primate.tree)) %<+% clades.df +
+      geom_text_clade(aes(label = clade), extend = 0.5) +
+      coord_geo_radial()
+  })
+})
